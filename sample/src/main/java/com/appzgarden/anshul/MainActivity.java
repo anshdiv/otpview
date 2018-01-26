e47958db4f3b0fb31c4f52be3cdb5a93874f3192
Anshul Gour <anshulgour.pgdav@gmail.com>
2018-01-27 01:49:24 +0530

    intial commit

---
+++ sample/src/main/java/com/appzgarden/anshul/MainActivity.java
@@ -0,0 +1,27 @@
+package com.appzgarden.anshul;
+
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.appzgarden.OtpView;
+import com.appzgarden.anshul.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        OtpView otpView= findViewById(R.id.otpview);
+        TextInputLayout textInputLayout = findViewById(R.id.textinputlayout);// use this to set error
+        otpView.setTextInputLayout(textInputLayout);
+        otpView.setOtpViewEventListener(new OtpView.OtpViewEventListener() {
+            @Override
+            public void onDataEntered(OtpView OtpView, boolean fromUser) {
+                // do something here
+            }
+        });
+
+    }
+}
